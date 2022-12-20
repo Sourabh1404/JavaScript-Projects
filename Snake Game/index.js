5ca8f21ff5891f032590144fa5ce32d1818c2df1
Sourabh70022 <sourabh70022@gmail.com>
2022-12-20 21:10:49 +0530

    Second

---
+++ Snake Game/index.js
@@ -0,0 +1,39 @@
+// Game variables and constants
+
+let direction= { x:0, y:0};
+const foodSound= new Audio('food.mp3');
+const gameoverSound= new Audio('gameover.mp3');
+const moveSound= new Audio('move.mp3');
+const musicSound= new Audio('music.mp3');
+
+
+
+
+
+// Game functions
+
+function main(ctime){
+    window.requestAnimationFrame(main);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  Main logic starts here
+window.requestAnimationFrame(main);
